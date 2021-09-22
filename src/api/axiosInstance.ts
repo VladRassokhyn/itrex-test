4bf5b3c841e362a4e14dad13e1c80e465725fdde
vladrassokhyn <perishvlad@gmail.com>
2021-09-22 20:46:33 +0300

    pages build init

--- src/api/axiosInstance.ts
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export const axiosInstance = axios.create({
-  baseURL: 'https://itrex-react-lab-files.s3.eu-central-1.amazonaws.com/',
-});
--- src/api/usersApi.ts
+++
@@ -1,8 +0,0 @@
-import { axiosInstance } from './axiosInstance';
-import { User } from '../store/types';
-
-export const usersApi = {
-  getUsers(): Promise<{ data: User[] }> {
-    return axiosInstance.get('react-test-api.json');
-  },
-};
